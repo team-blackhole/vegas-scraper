83518065b9fc6997e942ff547a4318943d94159d
Pelly Ryu <pelly.ryu@gmail.com>
2017-12-28 13:10:04 +0900

    feat: Request periodically with node-cron

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const request = require('request')
+// noinspection JSUnresolvedVariable
+const CronJob = require('cron').CronJob
 
-request('http://api.coinone.co.kr/ticker/?format=json&currency=iota', function (error, response, body) {
-  console.log('error:', error) // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-  console.log('body:', body) // Print the HTML for the Google homepage.
-})
+new CronJob('1 * * * * *', () => {
+  request('http://api.coinone.co.kr/ticker/?format=json&currency=iota', (error, response, body) => {
+    console.log('error:', error) // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+    console.log('body:', body) // Print the HTML for the Google homepage.
+  })
+}, null, true, 'Asia/Seoul')
